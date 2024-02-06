20bbf6f6bd9c7bf61ecf1b7758476a09d2853b64
CH B T G V Prasad <121245836+BHANUGANESH342@users.noreply.github.com>
2024-02-06 09:18:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+
+    // Typed.js initialization
+var typed = new Typed(".text", {
+    strings: ["computer vision . . .", "Machine Learning ...", "Programmer in AI ..."],
+    typeSpeed: 10,
+    backSpeed: 50,
+    backDelay: 1000,
+    loop: true
+});
